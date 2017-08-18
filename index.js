0c143e8c31279bc573c0feb5763bcc272e18377c
Jon Moss <me@jonathanmoss.me>
2017-08-17 22:47:47 -0400

    Basic graph

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile('index.html');
 })
 
 console.log('Starting server on port 3000...');
-app.listen(3000)
+app.listen(3000);
